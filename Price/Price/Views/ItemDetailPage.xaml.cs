56920b3b9fb59ea1a94255486fd8edb088a9bbcd
Oziel Guimaraes <contato@ozielguimaraes.net>
2017-03-02 19:24:37 -0400

    Add project files.

---
+++ Price/Price/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Price.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Price.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
